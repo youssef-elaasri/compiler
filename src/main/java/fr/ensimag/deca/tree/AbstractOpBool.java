bb3b8310a3f295fd7d85e27c391dd017d8c9eb1f
elaasriy <youssef.elaasri@grenoble-inp.org>
2023-12-29 12:58:33 +0100

    adding general functions for opBool

--- src/main/java/fr/ensimag/deca/tree/AbstractOpBool.java
+++ src/main/java/fr/ensimag/deca/tree/AbstractOpBool.java
@@ -5,6 +5,12 @@ import fr.ensimag.deca.DecacCompiler;
 import fr.ensimag.deca.context.ClassDefinition;
 import fr.ensimag.deca.context.ContextualError;
 import fr.ensimag.deca.context.EnvironmentExp;
+import fr.ensimag.ima.pseudocode.Label;
+import fr.ensimag.ima.pseudocode.Register;
+import fr.ensimag.ima.pseudocode.instructions.BEQ;
+import fr.ensimag.ima.pseudocode.instructions.BRA;
+import fr.ensimag.ima.pseudocode.instructions.CMP;
+import fr.ensimag.ima.pseudocode.instructions.LOAD;
 
 /**
  *
@@ -21,6 +27,20 @@ public abstract class AbstractOpBool extends AbstractBinaryExpr {
     public Type verifyExpr(DecacCompiler compiler, EnvironmentExp localEnv,
             ClassDefinition currentClass) throws ContextualError {
         throw new UnsupportedOperationException("not yet implemented");
+    }
+
+    /** ADDED CODE **/
+
+    protected void compareAndJump(int val, int registerNumber, Label label, DecacCompiler compiler) {
+        compiler.addInstruction(new CMP(val, Register.getR(registerNumber)));
+        compiler.addInstruction(new BEQ(label));
+    }
+
+    protected void trueLabel(int val, Label label, Label endlabel, DecacCompiler compiler) {
+        compiler.addLabel(label);
+        compiler.addInstruction(new LOAD(val,Register.getR(compiler.getStack().getCurrentRegister()-2)));
+        compiler.addInstruction(new BRA(endlabel));
+        compiler.getStack().decreaseRegister();
     }
 
 }
