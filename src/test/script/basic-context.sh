57e24d137382a0d3d34cc7d5d5b5bfed61010ecd
Jihad <Jihad.hammoud@grenoble-inp.org>
2024-01-02 17:26:43 +0100

    fixing bugs

--- src/test/script/basic-context.sh
+++ src/test/script/basic-context.sh
@@ -9,21 +9,80 @@ cd "$(dirname "$0")"/../../.. || exit 1
 
 PATH=./src/test/script/launchers:"$PATH"
 
-if test_context src/test/deca/context/invalid/provided/affect-incompatible.deca 2>&1 | \
-    grep -q -e 'affect-incompatible.deca:15:'
+
+# Test variable declarations
+
+if test_context src/test/deca/context/valid/provided/test_delcaration_var.deca 2>&1 | \
+    grep -q 'definition: variable defined at \[5, 8\], type=int' && grep -q 'definition: variable defined at \[5, 8\], type=ss'
 then
-    echo "Echec attendu pour test_context"
+    echo "PASSED"
 else
-    echo "Succes inattendu de test_context"
+    echo "FAILED"
     exit 1
 fi
 
-if test_context src/test/deca/context/valid/provided/hello-world.deca 2>&1 | \
-    grep -q -e 'hello-world.deca:[0-9]'
-then
-    echo "Echec inattendu pour test_context"
-    exit 1
-else
-    echo "Succes attendu de test_context"
-fi
 
+
+
+
+
+
+# if test_context src/test/deca/context/valid/provided/hello-world.deca 2>&1 | \
+#     grep -q -e 'hello-world.deca:[0-9]'
+# then
+#     echo "Echec inattendu pour test_context"
+#     exit 1
+# else
+#     echo "Succes attendu de test_context"
+# fi
+
+
+
+# # Testing Variable Declaration Feature
+
+# if test_context src/test/deca/context/invalid/provided/variable-deja-declare.deca 2>&1 | \
+#     grep -q -e '' # A Remplir
+# then
+#     echo "Echec attendu pour test_context"
+# else
+#     echo "Succes inattendu de test_context"
+#     exit 1
+# fi
+
+
+# if test_context src/test/deca/context/invalid/provided/not-initialized-variable.deca 2>&1 | \
+#     grep -q -e # A Remplir
+# then
+#     echo "Echec attendu pour test_context"
+# else
+#     echo "Succes inattendu de test_context"
+#     exit 1
+# fi
+
+# if test_context src/test/deca/context/invalid/provided/variable-not-used.deca 2>&1 | \
+#     grep -q -e # A Remplir
+# then
+#     echo "Echec attendu pour test_context"
+# else
+#     echo "Succes inattendu de test_context"
+#     exit 1
+# fi
+
+
+# if test_context src/test/deca/context/valid/provided/IntToFloat.deca 2>&1 | \
+#     grep -q -e # A remplir
+# then
+#     echo "Echec inattendu pour test_context"
+#     exit 1
+# else
+#     echo "Succes attendu de test_context"
+# fi
+
+# if test_context src/test/deca/context/valid/provided/affectationCompatible.deca 2>&1 | \
+#     grep -q -e # A remplir
+# then
+#     echo "Echec inattendu pour test_context"
+#     exit 1
+# else
+#     echo "Succes attendu de test_context"
+# fi
