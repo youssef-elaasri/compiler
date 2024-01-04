3d732619c9269b6d7d173e7d7c57896ee453937b
dehbiy <yakoubdehbi@grenoble-inp.org>
2024-01-04 17:50:18 +0100

    Fix: Quo division Can noww divide and integer by an integer

--- src/main/java/fr/ensimag/deca/tree/Divide.java
+++ src/main/java/fr/ensimag/deca/tree/Divide.java
@@ -5,6 +5,7 @@ import fr.ensimag.deca.DecacCompiler;
 import fr.ensimag.ima.pseudocode.BinaryInstructionDValToReg;
 import fr.ensimag.ima.pseudocode.Register;
 import fr.ensimag.ima.pseudocode.instructions.DIV;
+import fr.ensimag.ima.pseudocode.instructions.QUO;
 import fr.ensimag.ima.pseudocode.instructions.SUB;
 
 /**
@@ -37,9 +38,23 @@ public class Divide extends AbstractOpArith {
     protected void codeGenInst(DecacCompiler compiler) {
         int registerDec = compiler.getStack().getCurrentRegister() + 1 < compiler.getStack().getNumberOfRegisters() ?
                 1 : 0;
-        BinaryInstructionDValToReg binaryInstructionDValToReg = new DIV(Register.getR(compiler.getStack().getCurrentRegister() + registerDec -1),
-                Register.getR(compiler.getStack().getCurrentRegister() + registerDec));
-        codeGenInstOpArith(compiler,binaryInstructionDValToReg, true, true);
+        BinaryInstructionDValToReg binaryInstructionDValToReg;
+
+        if(this.getType().isFloat()) {
+            binaryInstructionDValToReg = new DIV(Register.getR(
+                    compiler.getStack().getCurrentRegister() + registerDec - 1),
+                    Register.getR(compiler.getStack().getCurrentRegister() + registerDec)
+            );
+        }
+        else {
+            binaryInstructionDValToReg = new QUO(Register.getR(
+                    compiler.getStack().getCurrentRegister() + registerDec - 1),
+                    Register.getR(compiler.getStack().getCurrentRegister() + registerDec)
+            );
+        }
+
+        codeGenInstOpArith(compiler, binaryInstructionDValToReg, true, true);
+
     }
 
 }
