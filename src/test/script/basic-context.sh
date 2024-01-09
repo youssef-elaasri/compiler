80af65324bbccb7d31935d887238eb50fe1265fb
Jihad <Jihad.hammoud@grenoble-inp.org>
2024-01-09 12:55:46 +0100

    Fixed some tests

--- src/test/script/basic-context.sh
+++ src/test/script/basic-context.sh
@@ -10,112 +10,75 @@ cd "$(dirname "$0")"/../../.. || exit 1
 PATH=./src/test/script/launchers:"$PATH"
 
 
+echo "\033[33mTesting Basic AST Decoration\033[0m"
+
+
 # Test variable declarations
 
-if test_context src/test/deca/context/valid/provided/test_declaration_int.deca 2>&1 | \
+if ! test_context src/test/deca/context/valid/provided/test_declaration_int.deca 2>&1 | \
     grep -q 'definition: variable defined at \[5, 8\], type=int'
 then
-    echo "PASSED"
-else
     echo "FAILED"
     exit 1
 fi
 
-if test_context src/test/deca/context/valid/provided/test_declaration_int.deca 2>&1 | \
+if ! test_context src/test/deca/context/valid/provided/test_declaration_int.deca 2>&1 | \
     grep -q 'definition: variable defined at \[7, 8\], type=int'
 then
-    echo "PASSED"
-else
     echo "FAILED"
     exit 1
 fi
 
 
-if test_context src/test/deca/context/valid/provided/test_declaration_float.deca 2>&1 | \
+if ! test_context src/test/deca/context/valid/provided/test_declaration_float.deca 2>&1 | \
     grep -q 'definition: variable defined at \[5, 10\], type=float'
 then
-    echo "PASSED"
-else
     echo "FAILED"
     exit 1
 fi
 
-if test_context src/test/deca/context/valid/provided/test_declaration_int.deca 2>&1 | \
+if ! test_context src/test/deca/context/valid/provided/test_declaration_int.deca 2>&1 | \
     grep -q '\[9, 4\] Assign'
 then
-    echo "PASSED"
-else
     echo "FAILED"
     exit 1
 fi
 
-if test_context src/test/deca/context/valid/provided/test_conv_Float.deca 2>&1 | \
+if ! test_context src/test/deca/context/valid/provided/test_conv_Float.deca 2>&1 | \
     grep -q 'ConvFloat'
 then
-    echo "PASSED"
-else
     echo "FAILED"
     exit 1
 fi
 
-
-
-# if test_context src/test/deca/context/valid/provided/hello-world.deca 2>&1 | \
-#     grep -q -e 'hello-world.deca:[0-9]'
-# then
-#     echo "Echec inattendu pour test_context"
-#     exit 1
-# else
-#     echo "Succes attendu de test_context"
-# fi
 
 
 
 # # Testing Variable Declaration Feature
 
-# if test_context src/test/deca/context/invalid/provided/variable-deja-declare.deca 2>&1 | \
-#     grep -q -e '' # A Remplir
-# then
-#     echo "Echec attendu pour test_context"
-# else
-#     echo "Succes inattendu de test_context"
-#     exit 1
-# fi
+if ! test_context src/test/deca/context/invalid/provided/variable-deja-declare.deca 2>&1 | \
+    grep -q -e 'Name a is already defined in localEnv !'
+then
+    echo "FAILED"
+    exit 1
+fi
 
 
-# if test_context src/test/deca/context/invalid/provided/not-initialized-variable.deca 2>&1 | \
-#     grep -q -e # A Remplir
-# then
-#     echo "Echec attendu pour test_context"
-# else
-#     echo "Succes inattendu de test_context"
-#     exit 1
-# fi
-
-# if test_context src/test/deca/context/invalid/provided/variable-not-used.deca 2>&1 | \
-#     grep -q -e # A Remplir
-# then
-#     echo "Echec attendu pour test_context"
-# else
-#     echo "Succes inattendu de test_context"
-#     exit 1
-# fi
+if ! test_context src/test/deca/context/invalid/provided/not-defined-variable.deca 2>&1 | \
+    grep -q -e  "Expression 'a' is not defined in the local environment"
+then
+    echo "FAILED"
+    exit 1
+fi
 
 
-# if test_context src/test/deca/context/valid/provided/IntToFloat.deca 2>&1 | \
-#     grep -q -e # A remplir
-# then
-#     echo "Echec inattendu pour test_context"
-#     exit 1
-# else
-#     echo "Succes attendu de test_context"
-# fi
 
-# if test_context src/test/deca/context/valid/provided/affectationCompatible.deca 2>&1 | \
-#     grep -q -e # A remplir
-# then
-#     echo "Echec inattendu pour test_context"
-#     exit 1
-# else
-#     echo "Succes attendu de test_context"
-# fi
+if ! test_context src/test/deca/context/valid/provided/affectationCompatible.deca 2>&1 | \
+    grep -q -e "6"
+then
+    echo "FAILED"
+    exit 1
+fi
+
+
+echo "\033[32mPASSED\033[0m"
