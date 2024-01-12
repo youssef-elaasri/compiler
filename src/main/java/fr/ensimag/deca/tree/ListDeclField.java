67a86f2f389e42c180de86daec952c0706e513e9
Jihad <Jihad.hammoud@grenoble-inp.org>
2024-01-12 16:11:24 +0100

    fix bugs

--- src/main/java/fr/ensimag/deca/tree/ListDeclField.java
+++ src/main/java/fr/ensimag/deca/tree/ListDeclField.java
@@ -1,16 +1,22 @@
 package fr.ensimag.deca.tree;
 
 import fr.ensimag.deca.DecacCompiler;
+import fr.ensimag.deca.context.ContextualError;
 import fr.ensimag.deca.tools.IndentPrintStream;
 
 public class ListDeclField extends TreeList<AbstractDeclField>{
     @Override
     public void decompile(IndentPrintStream s) {
-        throw new UnsupportedOperationException("not yet implemented");
+        for(AbstractDeclField f: getList()){
+            f.decompile(s);
+            s.println();
+        }
     }
 
-    public void verifyListDeclField(DecacCompiler compiler, AbstractIdentifier superId, AbstractIdentifier classId){
-        throw new UnsupportedOperationException("not yet implemented");
+    public void verifyListDeclField(DecacCompiler compiler, AbstractIdentifier superId, AbstractIdentifier classId) throws ContextualError{
+        for(AbstractDeclField field : this.getList()){
+            field.verifyField(compiler, superId, classId);
+        }
     }
 
 
