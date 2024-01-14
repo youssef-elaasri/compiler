d34b2453ce972dc6d80f5f4d3df6542d56130e05
dehbiy <yakoubdehbi@grenoble-inp.org>
2024-01-14 14:12:44 +0100

    Test: test for variable propagation provided

--- src/test/script/optimized_variables.sh
+++ src/test/script/optimized_variables.sh
@@ -8,12 +8,13 @@ NC='\033[0m' # No Color, to reset the color back to the default
 PASSED="PASSED"
 FAILED="FAILED"
 
+TESTNAME="optimized_variables"
 
 UNPROVIDED=src/test/deca/codegen/valid/unprovided
 
-rm -f "$UNPROVIDED"/classyClasses.ass  2>/dev/null
+rm -f "$UNPROVIDED"/"$TESTNAME".ass  2>/dev/null
 
-decac "$UNPROVIDED"/classyClasses.deca 2>/dev/null
+decac "$UNPROVIDED"/"$TESTNAME".deca 2>/dev/null
 
 # Check the exit status of the command
 if [ $? -ne 0 ]; then
@@ -21,11 +22,11 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-RESULT="$UNPROVIDED"/classyClasses.ass
+RESULT="$UNPROVIDED"/"$TESTNAME".ass
 
 # You can also check for multiple strings
 
-if cat "$RESULT" | grep -qE "init.Father:|init.Mother:"; then
+if cat "$RESULT" | grep -qE "LOAD #42, R2|STORE R2, 1(GB)|LOAD #0x1.5p5, R2|STORE R2, 2(GB)"; then
     echo -e "${GREEN}${PASSED}${NC}"
     exit 0
 else
