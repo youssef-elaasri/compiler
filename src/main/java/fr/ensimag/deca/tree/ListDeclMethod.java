e069f55963231402d5dd81a4c0b4166bd761b61e
Jihad <Jihad.hammoud@grenoble-inp.org>
2024-01-15 13:13:38 +0100

    Add  New

--- src/main/java/fr/ensimag/deca/tree/ListDeclMethod.java
+++ src/main/java/fr/ensimag/deca/tree/ListDeclMethod.java
@@ -1,11 +1,15 @@
 package fr.ensimag.deca.tree;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import fr.ensimag.deca.DecacCompiler;
 import fr.ensimag.deca.context.ClassDefinition;
 import fr.ensimag.deca.context.ContextualError;
 import fr.ensimag.deca.context.EnvironmentExp;
 import fr.ensimag.deca.context.ExpDefinition;
 import fr.ensimag.deca.tools.IndentPrintStream;
+import fr.ensimag.deca.tools.SymbolTable;
 
 public class ListDeclMethod extends TreeList<AbstractDeclMethod> {
     @Override
@@ -16,10 +20,19 @@ public class ListDeclMethod extends TreeList<AbstractDeclMethod> {
        }
 
     }
-    public void verifyListDeclMethod(DecacCompiler compiler, AbstractIdentifier superId) throws ContextualError{
+    public EnvironmentExp verifyListDeclMethod(DecacCompiler compiler, AbstractIdentifier superId) throws ContextualError{
+        EnvironmentExp envExpr =  new EnvironmentExp(null);
         for(AbstractDeclMethod meth:this.getList()){
-            meth.verifyMethod(compiler, superId);
+            EnvironmentExp envExp = meth.verifyMethod(compiler, superId);
+            Map<SymbolTable.Symbol, ExpDefinition> interMap = new HashMap<>(envExp.getExpDefinitionMap());
+            /*Vérifier si les deux environnements sont disjoints*/
+            interMap.keySet().retainAll(envExpr.getExpDefinitionMap().keySet());
+            if (!interMap.isEmpty()) {
+                throw new ContextualError("Vous avez déclaré un champ plusieurs fois dans la classe !", this.getLocation());
+            }
+            envExpr.getExpDefinitionMap().putAll(envExp.getExpDefinitionMap());
         }
+        return envExpr;
     }
     public void verifyListDeclMethodBody(DecacCompiler compiler, ExpDefinition localEnv, ClassDefinition classId){
         throw new UnsupportedOperationException("not yet implemented");
---
+++ src/main/java/fr/ensimag/deca/tree/New.java
@@ -0,0 +1,50 @@
+package fr.ensimag.deca.tree;
+
+import fr.ensimag.deca.DecacCompiler;
+import fr.ensimag.deca.context.ClassDefinition;
+import fr.ensimag.deca.context.ContextualError;
+import fr.ensimag.deca.context.EnvironmentExp;
+import fr.ensimag.deca.context.Type;
+import fr.ensimag.deca.tools.IndentPrintStream;
+import fr.ensimag.ima.pseudocode.Label;
+import fr.ensimag.ima.pseudocode.Register;
+import fr.ensimag.ima.pseudocode.RegisterOffset;
+import fr.ensimag.ima.pseudocode.instructions.*;
+
+import java.io.PrintStream;
+
+public class New extends AbstractExpr{
+    AbstractIdentifier classNanme;
+
+    public New(AbstractIdentifier abstractIdentifier){
+        super();
+        classNanme = abstractIdentifier;
+    }
+
+
+    @Override
+    public Type verifyExpr(DecacCompiler compiler, EnvironmentExp localEnv, ClassDefinition currentClass) throws ContextualError {
+        return null;
+    }
+
+    @Override
+    public void decompile(IndentPrintStream s) {
+
+    }
+
+    @Override
+    protected void prettyPrintChildren(PrintStream s, String prefix) {
+
+    }
+
+    @Override
+    protected void iterChildren(TreeFunction f) {
+
+    }
+
+    @Override
+    protected void codeGenInst(DecacCompiler compiler) {
+    }
+
+
+}
