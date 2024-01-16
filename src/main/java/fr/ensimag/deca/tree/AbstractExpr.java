8041413a59510921f70f87cff64eb36fe5c58651
Jihad <Jihad.hammoud@grenoble-inp.org>
2024-01-16 12:45:38 +0100

    Fixed bug related to New

--- src/main/java/fr/ensimag/deca/tree/AbstractExpr.java
+++ src/main/java/fr/ensimag/deca/tree/AbstractExpr.java
@@ -85,9 +85,10 @@ public abstract class AbstractExpr extends AbstractInst {
 //        throw new UnsupportedOperationException("not yet implemented");
         Type currentType = this.verifyExpr(compiler, localEnv, currentClass);
         if (!(expectedType.isFloat() && currentType.isInt())) {
-            if (!(currentType.getClass().isAssignableFrom(expectedType.getClass()))) {
-                throw new ContextualError("assign_compatible condition in rvalue no-terminal fails !: Trying to assign " + currentType + " to " + expectedType, this.getLocation());
-            }
+            throw new ContextualError("assign_compatible condition in rvalue no-terminal fails !: Trying to assign " + currentType + " to " + expectedType, this.getLocation());
+        }
+        if (!(currentType.getClass().isAssignableFrom(expectedType.getClass()))) {
+            throw new ContextualError("assign_compatible condition in rvalue no-terminal fails !: Trying to assign " + currentType + " to " + expectedType, this.getLocation());
         }
         if (expectedType.isFloat() && currentType.isInt()) {
             AbstractExpr convF = new ConvFloat(this);
