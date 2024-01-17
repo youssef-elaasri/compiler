7083a3726dc2c76ddd8bfaa12ba41183876fbd43
Jihad <Jihad.hammoud@grenoble-inp.org>
2024-01-17 18:24:10 +0100

    Implemented DeclParam passe 3

--- src/main/java/fr/ensimag/deca/tree/AbstractDeclParam.java
+++ src/main/java/fr/ensimag/deca/tree/AbstractDeclParam.java
@@ -2,12 +2,13 @@ package fr.ensimag.deca.tree;
 
 import fr.ensimag.deca.DecacCompiler;
 import fr.ensimag.deca.context.ContextualError;
+import fr.ensimag.deca.context.EnvironmentExp;
 import fr.ensimag.deca.context.ExpDefinition;
 import fr.ensimag.deca.context.Type;
 
 public abstract class AbstractDeclParam extends Tree{
     protected abstract Type verifyParam(DecacCompiler compiler) throws ContextualError;
-    protected abstract ExpDefinition verifyParamName(DecacCompiler compiler);
+    protected abstract EnvironmentExp verifyParamName(DecacCompiler compiler) throws ContextualError;
     protected abstract Type getType();
 
 }
--- src/main/java/fr/ensimag/deca/tree/ListDeclParam.java
+++ src/main/java/fr/ensimag/deca/tree/ListDeclParam.java
@@ -3,13 +3,17 @@ package fr.ensimag.deca.tree;
 import fr.ensimag.deca.DecacCompiler;
 import fr.ensimag.deca.context.ContextualError;
 import fr.ensimag.deca.context.EnvironmentExp;
+import fr.ensimag.deca.context.ExpDefinition;
 import fr.ensimag.deca.context.Signature;
 import fr.ensimag.deca.context.Signature;
 import fr.ensimag.deca.context.Type;
 import fr.ensimag.deca.tools.IndentPrintStream;
+import fr.ensimag.deca.tools.SymbolTable;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Set;
 
 public class ListDeclParam extends TreeList<AbstractDeclParam> {
 
@@ -33,8 +37,18 @@ public class ListDeclParam extends TreeList<AbstractDeclParam> {
         return sig;
     }
 
-    public void verifyListParamName(DecacCompiler compiler){
-        throw new UnsupportedOperationException("not yet implemented");
+    public void verifyListParamName(DecacCompiler compiler) throws ContextualError{
+        EnvironmentExp envExpr = new EnvironmentExp(null);
+        for(AbstractDeclParam param:this.getList()){
+            EnvironmentExp exp = param.verifyParamName(compiler);
+            Set<SymbolTable.Symbol> keysExp =exp.getExpDefinitionMap().keySet();
+            Set<SymbolTable.Symbol> keysExpR = envExpr.getExpDefinitionMap().keySet();
+            if (!Collections.disjoint(keysExp, keysExpR)) {
+                throw new ContextualError("Vous avez déclaré " + keysExp + " plusieurs fois dans la classe !", param.getLocation());
+            }
+            envExpr.getExpDefinitionMap().putAll(exp.getExpDefinitionMap());
+
+        }
     }
 
 }
