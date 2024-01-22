2d78f875b6545f482e8116e6428395f04966dc36
elaasriy <youssef.elaasri@grenoble-inp.org>
2024-01-22 14:56:26 +0100

    fixing init bugs

--- src/main/java/fr/ensimag/deca/tree/Lower.java
+++ src/main/java/fr/ensimag/deca/tree/Lower.java
@@ -2,12 +2,8 @@ package fr.ensimag.deca.tree;
 
 
 import fr.ensimag.deca.DecacCompiler;
-import fr.ensimag.ima.pseudocode.BranchInstruction;
-import fr.ensimag.ima.pseudocode.Label;
 import fr.ensimag.ima.pseudocode.Register;
 import fr.ensimag.ima.pseudocode.UnaryInstructionToReg;
-import fr.ensimag.ima.pseudocode.instructions.BEQ;
-import fr.ensimag.ima.pseudocode.instructions.BLT;
 import fr.ensimag.ima.pseudocode.instructions.SLT;
 
 /**
